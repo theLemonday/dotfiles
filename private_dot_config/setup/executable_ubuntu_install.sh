51b361c792323cf074a2d7cd88a2ea18f4e73ccc
theLemonday <thearchitecturer@gmail.com>
2022-05-17 21:14:10 +0700

    backup init

--- private_dot_config/setup/executable_ubuntu_install.sh
+++ private_dot_config/setup/executable_ubuntu_install.sh
@@ -2,6 +2,6 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
 
-sudo apt-get install kitty sqlite3 build-essential unzip golang zsh ctags python3 -y
+sudo apt-get install kitty sqlite3 build-essential unzip golang zsh universal-ctags python3 -y
 
 sudo apt install --yes -- python3-venv
