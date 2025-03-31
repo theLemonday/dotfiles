e231f3e62cef423b571c04d82026c2ec1bfd25b8
Lemonday <nhathao090703@gmail.com>
2025-03-31 10:08:33 +0700

    Updated Mon Mar 31 10:08:33 +07 2025

--- modules/lazydocker.nix
+++ modules/lazydocker.nix
@@ -4,20 +4,23 @@ let
 in
 {
   home.packages = [
-    (pkgs.buildGoModule
-      rec {
-        pname = "lazydocker";
-        inherit version;
-
-        src = pkgs.fetchFromGitHub {
-          owner = "jesseduffield";
-          repo = "lazydocker";
-          rev = "v${version}";
-          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-        };
-
-        vendorHash = "sha256-cVjDdrxmGt+hj/WWP9B3BT739k9SSr4ryye5qWb3XNM=";
-      })
+    pkgs.lazydocker
+    # (pkgs.buildGoModule
+    #   rec {
+    #     pname = "lazydocker";
+    #     inherit version;
+    #
+    #     src = pkgs.fetchFromGitHub
+    #       {
+    #         owner = "jesseduffield";
+    #         repo = "lazydocker";
+    #         rev = "v${version}";
+    #         hash = "sha256-cVjDdrxmGt+hj/WWP9B3BT739k9SSr4ryye5qWb3XNM=";
+    #       };
+    #
+    #     vendorHash = "";
+    #     # vendorHash = "sha256-cVjDdrxmGt+hj/WWP9B3BT739k9SSr4ryye5qWb3XNM=";
+    #   })
   ];
 
 }
