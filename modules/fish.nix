d225677d990eccc7cfde72d3da3966a5fcd0d261
Lemonday <nhathao090703@gmail.com>
2025-04-09 10:19:58 +0700

    Updated Wed 09 Apr 2025 10:19:58 AM +07

--- modules/fish.nix
+++ modules/fish.nix
@@ -97,7 +97,10 @@ in
       kubecolorSetup
     ];
     plugins = [
-      { name = "tide"; src = pkgs.fishPlugins.tide; }
+      {
+        name = "tide";
+        src = pkgs.fishPlugins.tide;
+      }
     ];
   };
 }
