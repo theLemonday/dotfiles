2064d7800b7ff1aaa4cfbb294436857bc46fd809
Lemonday <nhathao090703@gmail.com>
2025-04-09 10:33:16 +0700

    Updated Wed 09 Apr 2025 10:33:16 AM +07

--- home.nix
+++ home.nix
@@ -83,6 +83,7 @@ in
     pkgs.kubecolor
 
     pkgs.openstackclient
+    pkgs.fishPlugins.tide
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
