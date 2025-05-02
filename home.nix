8cc23453e7aae2d8b6980f1425fec375f481443c
Lemonday <nhathao090703@gmail.com>
2025-05-02 21:52:21 +0700

    Updated Fri May  2 09:52:21 PM +07 2025

--- home.nix
+++ home.nix
@@ -167,12 +167,12 @@ in
     ls = "eza";
     ll = "ls-l";
     k = "kubectl";
-    update = "sudonixos-rebuildswitch";
+    update = "sudo nixos-rebuild switch";
     lzd = "lazydocker";
     lzg = "lazygit";
     n = "nvim";
-    nf = "nvim$(fzf)";
-    pnpx = "pnpmdlx";
+    nf = "nvim $(fzf)";
+    pnpx = "pnpm dlx";
     hm = "home-manager";
     hms = "$HOME/.config/home-manager/update-home.fish";
     tf = "terraform";
