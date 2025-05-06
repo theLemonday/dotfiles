ee89eecf831cf5ca06510b0a1dad6fc2f78d19d5
Lemonday <nhathao090703@gmail.com>
2025-05-06 14:27:47 +0700

    Updated Tue May  6 02:27:47 PM +07 2025

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    # nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
