0b0ffc450d458685f88f5bee83fd3f502c007594
theLemonday <nhathao090703@gmail.com>
2025-07-24 23:38:08 +0700

    Updated Thu Jul 24 11:38:08 PM +07 2025

--- home.nix
+++ home.nix
@@ -26,7 +26,7 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs;[
-    anki
+    anki-bin
     # Graphics
     nixgl.nixGLIntel
 
