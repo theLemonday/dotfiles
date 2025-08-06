6252d020e9b63aea534f50f881f96dc262540004
theLemonday <nhathao090703@gmail.com>
2025-08-06 09:44:23 +0700

    Updated Wed Aug  6 09:44:23 AM +07 2025

--- modules/plasma_manager.nix
+++ modules/plasma_manager.nix
@@ -7,7 +7,28 @@
       {
         location = "top";
         height = 26;
+        widgets = [
+          # We can configure the widgets by adding the name and config
+          # attributes. For example to add the the kickoff widget and set the
+          # icon to "nix-snowflake-white" use the below configuration. This will
+          # add the "icon" key to the "General" group for the widget in
+          # ~/.config/plasma-org.kde.plasma.desktop-appletsrc.
+          {
+            name = "org.kde.plasma.kickoff";
+            config = {
+              General = {
+                icon = "nix-snowflake-white";
+                alphaSort = true;
+              };
+            };
+          }
+          "org.kde.plasma.icontasks"
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.digitalclock"
+        ];
       }
+
     ];
 
     hotkeys = {
