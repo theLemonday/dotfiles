cffcae477478aad17c3421906d46cbde51f771ba
theLemonday <nhathao090703@gmail.com>
2025-11-06 14:47:45 +0700

    Updated Thu Nov  6 02:47:45 PM +07 2025

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -1,4 +1,12 @@
 { lib, pkgs, ... }:
+let
+  k9sSrc = pkgs.fetchFromGitHub {
+    owner = "derailed";
+    repo = "k9s";
+    rev = "master";
+    sha256 = "sha256-PYaVzUAQuy5LBkyJ3otWX1iRYWSkt4sD3HIvpGTOiQY="; # replace after first build
+  };
+in
 {
   home.packages = with pkgs;[
     kubectl
@@ -7,6 +15,10 @@
     # helm
   ];
 
+  home.file.".config/k9s/skins" = {
+    source = "${k9sSrc}/skins";
+    recursive = true;
+  };
   programs.k9s = {
     enable = true;
   };
