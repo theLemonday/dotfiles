c6097a77526122e6b29abc4bf074860e77eade77
theLemonday <nhathao090703@gmail.com>
2025-11-10 20:47:29 +0700

    Updated Mon Nov 10 08:47:29 PM +07 2025

--- modules/llm.nix
+++ modules/llm.nix
@@ -1,11 +1,1 @@
-{
-  # in your home.nix
-  services.ollama = {
-    enable = true;
-    # To enable acceleration, you might need to specify your hardware.
-    # For NVIDIA:
-    acceleration = "cuda";
-    # For AMD (more experimental):
-    # acceleration = "rocm";
-  };
-}
+{ }
