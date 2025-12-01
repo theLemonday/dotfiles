8d81fb3b226cf3dec42282f6967bfc795ee52817
theLemonday <nhathao090703@gmail.com>
2025-12-01 21:49:47 +0700

    Updated Mon Dec  1 09:49:47 PM +07 2025

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }: {
   home.packages = with pkgs;[ pure-prompt ];
 
   programs.zsh = {
@@ -89,6 +89,12 @@
 
   programs.fzf.enableZshIntegration = true;
 
+  home.activation.zshCompile = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+    # Compile .zshrc if it exists
+    if [ -f "${config.home.homeDirectory}/.zshrc" ]; then
+      zsh -fc "zcompile ${config.home.homeDirectory}/.zshrc"
+    fi
+  '';
   # programs.starship = { enable = true; enableZshIntegration = true; };
 }
 
