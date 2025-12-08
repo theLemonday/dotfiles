fcb331cdc67ba7affd45357fda3db4cc7306f77f
theLemonday <nhathao090703@gmail.com>
2025-12-08 14:17:19 +0700

    regular update

--- modules/git.nix
+++ modules/git.nix
@@ -8,44 +8,37 @@
   programs.git = {
     enable = true;
     settings = {
-      user = {
-        name = "theLemonday";
-        email = "nhathao090703@gmail.com";
-      };
       pull.rebase = true;
-      # rebase.autoStash = true;
-      # # Optional: helps with those repetitive conflicts we talked about
-      # rerere.enabled = true;
-    };
-
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-
-    delta = {
-      enable = true;
-    };
-
-    aliases = {
-      co = "checkout";
-      br = "branch";
-      cm = "commit -m";
-      st = "status";
+      user =
+        {
+          name = "theLemonday";
+          email = "nhathao090703@gmail.com";
+        };
+      alias = {
+        co = "checkout";
+        br = "branch";
+        cm = "commit -m";
+        st = "status";
+      };
+      init = {
+        defaultBranch = "main";
+      };
     };
   };
+
+  programs.delta = { enable = true; enableGitIntegration = true; };
 
   programs.lazygit = {
     enable = true;
 
     settings = {
       git = {
-        pagers = [
-          {
-            colorArg = "always";
-            pager = "delta --dark --paging=never";
-          }
-        ];
+        paging = {
+          colorArg = "always";
+          pager = "delta --dark --paging=never";
+        };
       };
     };
   };
+
 }
