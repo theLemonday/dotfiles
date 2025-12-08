f51b9f03789a00f8c5840b46fce972eb8e7d3cfa
theLemonday <nhathao090703@gmail.com>
2025-12-08 08:48:07 +0700

    Updated Mon Dec  8 08:48:07 AM +07 2025

--- home.nix
+++ home.nix
@@ -114,6 +114,10 @@ in
   home.sessionVariables = {
     EDITOR = "vi";
     XMODIFIERS = "@im=fcitx";
+
+    SSH_ASKPASS = "/usr/bin/ksshaskpass";
+    # Force SSH to use the GUI prompt even if you are in a terminal
+    SSH_ASKPASS_REQUIRE = "prefer";
   };
 
   home.sessionPath = [
