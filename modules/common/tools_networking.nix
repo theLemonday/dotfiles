0bd421fd0a6c2c78f357c1fcbbded84bfae2a87d
southclementide <nhathao090703@gmail.com>
2026-02-13 21:45:22 +0700

    Updated Fri Feb 13 09:45:22 PM +07 2026

--- modules/common/tools_networking.nix
+++ modules/common/tools_networking.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     iproute2
@@ -6,10 +6,6 @@
     websocat
     nmap
     inetutils
+    bind
   ];
-
-  # home.activation.containerlabFishCompletion = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-  #   mkdir -p "$HOME/.config/fish/completions"
-  #   # ${pkgs.containerlab}/bin/containerlab completion fish > "$HOME/.config/fish/completions/containerlab.fish"
-  # '';
 }
