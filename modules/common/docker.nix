b5f841f38627fcf90e67bd5a15fb9d860a504aff
southclementide <nhathao090703@gmail.com>
2026-02-19 19:39:18 +0700

    Updated Thu Feb 19 07:39:18 PM +07 2026

--- modules/common/docker.nix
+++ modules/common/docker.nix
@@ -8,4 +8,20 @@
     hadolint
     nerdctl
   ];
+  nixpkgs.overlays = [
+    (final: prev: {
+      nerdctl = prev.ko.overrideAttrs (oldAttrs: {
+        # 1. Add the utility that provides 'installShellCompletion'
+        nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ final.installShellFiles ];
+
+        # 2. Run the command after the main install phase
+        postInstall = (oldAttrs.postInstall or "") + ''
+          # The command you suggested:
+          installShellCompletion --cmd nerdctl \
+            --zsh <($out/bin/nerdctl completion zsh) 
+        '';
+      });
+    })
+  ];
+
 }
