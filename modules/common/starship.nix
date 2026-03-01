57d60303756b1e6fbe26868f4877012dd065c5f5
southclementide <nhathao090703@gmail.com>
2026-03-01 22:55:00 +0700

    Updated Sun Mar  1 10:55:00 PM +07 2026

--- modules/common/starship.nix
+++ modules/common/starship.nix
@@ -5,7 +5,7 @@
 
     # Configure the look right here in Nix!
     settings = {
-      format = "$directory$git_branch$fill$kubernetes\n$character";
+      format = "$all$fill$kubernetes\n$character";
       fill = {
         symbol = " ";
       };
@@ -24,11 +24,11 @@
         format = "[$symbol$context(\($namespace\))]($style)";
       };
 
-      python = {
-        detect_files = [ "requirements.txt" "pyproject.toml" ".python-version" ];
-        detect_folders = [ ".venv" "venv" ];
-        format = "via [\${symbol}\${pyenv_prefix}(\${version} )(\\($virtualenv\\))]($style) ";
-      };
+      # python = {
+      #   detect_files = [ "requirements.txt" "pyproject.toml" ".python-version" ];
+      #   detect_folders = [ ".venv" "venv" ];
+      #   format = "via [\${symbol}\${pyenv_prefix}(\${version} )(\\($virtualenv\\))]($style) ";
+      # };
     };
   };
 }
