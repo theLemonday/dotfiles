5d58a17d0a77dc2afcfb7bc7bb2c15a01ba5fb58
southclementide <nhathao090703@gmail.com>
2026-03-30 02:59:37 +0700

    Updated Mon Mar 30 02:59:37 AM +07 2026

--- modules/common/neovim.nix
+++ modules/common/neovim.nix
@@ -8,6 +8,6 @@
     #    extraLuaPackages = luaPkgs: with luaPkgs; [ mimetypes ];
     viAlias = true;
     vimAlias = true;
-    # package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
 }
